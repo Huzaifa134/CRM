beb0540f9428328cb69735ede0e235578a94165e
unknown <astraldevelopers0@gmail.com>
2024-05-16 12:43:14 +0500

    queries pages pull

--- src/Pages/Queries/pages/Mail.jsx
+++ src/Pages/Queries/pages/Mail.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react'
+import { GoMail } from "react-icons/go";
+import { FaInfoCircle } from "react-icons/fa";
 
-function Mail() {
-  return <div>mail</div>;
+const Mail = () => {
+  return (
+    <>
+      <div className='bg-[#cfd7df] m-3 p-4 flex gap-4'>
+        <button className='text-sm bg-[#12344d] text-white p-2 flex items-center gap-1 rounded-md font-semibold px-5 hover:bg-[#1699dd]'><GoMail />Compose</button>
+        <button className='text-sm text-[#12344d] bg-white border-[1.5px] border-[#b1b0b0] p-2 px-5 rounded-md font-semibold flex items-center gap-1'><FaInfoCircle />anas@travbizz.com</button>
+      </div>
+    </>
+  )
 }
 
-export default Mail;
+export default Mail
