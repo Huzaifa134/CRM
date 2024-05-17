8cde5080a7c1d8baa4e63428584c4f8831492fd1
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-05-17 16:14:16 +0500

    edits done

--- src/Pages/Itineraries/menu/BasicMenu.jsx
+++ src/Pages/Itineraries/menu/BasicMenu.jsx
@@ -5,8 +5,6 @@ import MenuItem from '@mui/material/MenuItem';
 import { HiDotsVertical } from "react-icons/hi";
 import TextField from "@mui/material/TextField";
 import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import CloseIcon from "@mui/icons-material/Close";
 import Modal from '@mui/material/Modal';
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -15,17 +13,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { useState } from 'react';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -79,9 +67,8 @@ export default function BasicMenu() {
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItem ><Button onClick={handleOpen}>Iltenaries</Button></MenuItem>
-        <MenuItem >My account</MenuItem>
-        <MenuItem >Logout</MenuItem>
+        <MenuItem ><Button onClick={handleOpen}>Edit Iltenaries</Button></MenuItem>
+        <MenuItem >Duplicate</MenuItem>
       
       </Menu>
       <Modal
